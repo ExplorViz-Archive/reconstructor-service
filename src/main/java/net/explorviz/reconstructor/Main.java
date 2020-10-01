e4228a00556a5e48b61300204bf1d8777a77ac18
Kevin Lotz <lotzk@outlook.com>
2020-10-01 09:12:34 +0200

    Remove cassandra related classes

--- src/main/java/net/explorviz/reconstructor/Main.java
+++ src/main/java/net/explorviz/reconstructor/Main.java
@@ -3,24 +3,20 @@ package net.explorviz.reconstructor;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
-import net.explorviz.reconstructor.persistence.cassandra.DBHelper;
 import net.explorviz.reconstructor.stream.ReconstructionStream;
 
 @QuarkusMain
 public class Main implements QuarkusApplication {
 
 
-  private final DBHelper db;
   private final ReconstructionStream stream;
 
-  public Main(final DBHelper db, final ReconstructionStream stream) {
-    this.db = db;
+  public Main(final ReconstructionStream stream) {
     this.stream = stream;
   }
 
   @Override
   public int run(final String... args) throws Exception {
-    this.db.initialize();
     this.stream.getStream().cleanUp();
     this.stream.getStream().start();
 
